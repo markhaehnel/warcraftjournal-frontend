bbfac6dd3324c5c40167a4e271ae2da3f7bfe05f
Mark Hähnel <hello@markhaehnel.de>
2017-06-11 16:10:56 +0200

    Removed unneeded files

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="text-centered welcome">
-        
         <h1>Welcome to The Warcraft Journal.</h1>
         <p>Statistics and calculations on World of Warcraft guild and player data.</p>
     </div>
