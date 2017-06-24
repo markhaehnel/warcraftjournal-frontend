ec3ee8bf694676a2a92032436679bbee6268a88c
Mark Hähnel <hello@markhaehnel.de>
2017-06-24 15:16:16 +0200

    Added affix button to startpage

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,8 @@
     <div class="text-center welcome">
         <h1>Welcome to The Warcraft Journal.</h1>
         <p>Statistics and calculations on World of Warcraft guild and player data.</p>
-        <nuxt-link class="btn btn-primary" to="/buildings">Broken Shore Buildings</nuxt-link>
+        <nuxt-link class="btn btn-primary mr-3" to="/buildings">Broken Shore Buildings</nuxt-link>
+        <nuxt-link class="btn btn-primary" to="/affixes">Mythic+ Affixes</nuxt-link>
     </div>
 </template>
 
