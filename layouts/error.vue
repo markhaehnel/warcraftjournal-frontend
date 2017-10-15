69739cdd9a8af9da876e19936b65a5f2b71fa64b
Mark Hähnel <hello@markhaehnel.de>
2017-10-15 13:16:30 +0200

    Prettified error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,18 +1,37 @@
 <template>
     <div>
-        <h1>
-            {{ error.statusCode }}
-        </h1>
-        <h2>
-            {{ error.message }}
-        </h2>
-        <nuxt-link to="/" v-if="error.statusCode === 404">
-            Homepage
-        </nuxt-link>
+        <div class="site-content">
+            <site-header></site-header>
+            <div class="hero is-fullheight is-danger is-bold">
+                <div class="hero-body">
+                    <div class="container has-text-centered">
+                        <h1 class="title">Error {{ error.statusCode }}</h1>
+                        <h2 class="subtitle">{{ error.message }}</h2>
+                        <nuxt-link to="/" v-if="error.statusCode === 404">
+                            Homepage
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <site-footer></site-footer>
     </div>
 </template>
+
 <script>
+import SiteHeader from '~/components/Header.vue'
+import SiteFooter from '~/components/Footer.vue'
+
 export default {
-    props: ['error']
+    props: ['error'],
+    components: {
+        SiteHeader,
+        SiteFooter
+    }
 }
 </script>
+
+<style lang="less">
+.site-content {
+    min-height: 100vh;
+}
