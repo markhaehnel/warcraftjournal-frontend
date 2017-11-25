a295a38c79c3d3bc3fc1a98a2162c49699f1b66c
Mark Hähnel <hello@markhaehnel.de>
2017-11-25 16:05:14 +0100

    Added quick links to homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,46 @@
 <template>
-    <div class="hero is-fullheight is-dark is-warrior">
-        <div class="hero-body">
-            <div class="container has-text-centered">
-                <h1 class="title">
-                    <strong>Welcome to The Warcraft Journal.</strong>
-                </h1>
-                <h2 class="subtitle">
-                    Statistics and calculations on World of Warcraft guild and player data.
-                </h2>
+    <div>
+        <div class="hero is-large is-dark is-shaman">
+            <div class="hero-body">
+                <div class="container has-text-centered">
+                    <h1 class="title">
+                        <strong>Welcome to The Warcraft Journal.</strong>
+                    </h1>
+                    <h2 class="subtitle">
+                        Statistics and calculations on World of Warcraft guild and player data.
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="hero">
+            <div class="hero-body">
+                <div class="container">
+                    <div class="columns">
+                        <nuxt-link class="column has-text-centered" to="/guilds">
+                            <b-icon icon="account-multiple" size="is-large"></b-icon>
+                            <p class="title is-4">Guild Lookup</p>
+                            <p class="subtitle">Compare and evaluate gear</p>
+                        </nuxt-link>
+                        <nuxt-link class="column has-text-centered" to="/token">
+                            <b-icon icon="coins" size="is-large"></b-icon>
+                            <p class="title is-4">WoW Token</p>
+                            <p class="subtitle">Current and historical token prices</p>
+                        </nuxt-link>
+                        <nuxt-link class="column has-text-centered" to="/mythicplus/affixes">
+                            <b-icon icon="format-list-numbers" size="is-large"></b-icon>
+                            <p class="title is-4">Mythic+</p>
+                            <p class="subtitle">Compare and evaluate gear</p>
+                        </nuxt-link>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+a:hover > .title {
+  text-decoration: underline;
+}
+</style>
