485bac10a46a2ae9f0f122a3b72af2b28aa138ae
Mark Hähnel <hello@markhaehnel.de>
2017-11-25 16:04:37 +0100

    Added newsletter component

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,24 +1,30 @@
 <template>
-    <footer class="footer">
-        <div class="container">
-            <div class="content">
-                <p class="has-text-centered"><a href="#top" v-smooth-scroll><b-icon icon="expand_less" type="is-dark hover-up" size="is-large"></b-icon></a></p>
-                <p>&copy;
-                    <strong>The Warcraft Journal</strong> {{year}}</p>
-                <p class="has-text-justified">
-                    World of Warcraft, Warcraft, Battle.net and Blizzard Entertainment are trademarks or registered trademarks of Blizzard Entertainment, Inc. in the U.S. and/or other countries.
-                </p>
+    <div>
+        <newsletter></newsletter>
+        <footer class="footer">
+            <div class="container">
+                <div class="content">
+                    <p class="has-text-centered"><a href="#top" v-smooth-scroll><b-icon icon="expand_less" type="is-dark hover-up" size="is-large"></b-icon></a></p>
+                    <p>&copy;
+                        <strong>The Warcraft Journal</strong> {{year}}</p>
+                    <p class="has-text-justified">
+                        World of Warcraft, Warcraft, Battle.net and Blizzard Entertainment are trademarks or registered trademarks of Blizzard Entertainment, Inc. in the U.S. and/or other countries.
+                    </p>
+                </div>
             </div>
-        </div>
-    </footer>
+        </footer>
+    </div>
 </template>
 
 <script>
+import Newsletter from '~/components/Newsletter.vue'
+
 export default {
     data () {
         return {
             year: new Date().getFullYear()
         }
-    }
+    },
+    components: { Newsletter }
 }
 </script>
---
+++ components/Newsletter.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="hero is-info">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns is-vcentered">
+                    <div class="column is-one-third is-left">
+                        <p class="title">TWJ Newsletter</p>
+                        <p class="subtitle">Signup for the latest news and updates.</p>
+                    </div>
+                    <div class="column">
+                        <form>
+                            <div class="field is-grouped">
+                                <div class="control has-icons-left is-expanded">
+                                    <b-icon icon="email"></b-icon>
+                                    <input type="email" class="input" placeholder="email address">
+                                </div>
+                                <div class="control">
+                                    <input type="submit" value="Subscribe" class="button is-white is-outlined">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
